ec40067994aec4ef496c8c337382eb7d5d086831
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-10-02 21:59:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 // write your code below!
 happyHolidays = () => {return "Happy holidays!";}
 
-happyHolidaysTo(name){
-  return "Happy holidays" + " ${name}!"
-}
-
-happy
+happyHolidaysTo = (name) => {return "Happy holidays" + " ${name}!"}
